5a58707c5e9e84b84502854aca6eefed212eb4a9
AndreyRadionov <andyomsk@gmail.com>
2018-04-15 19:23:15 +0600

    Gradle Test Task Added

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityGCloudTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityGCloudTest.java
@@ -14,6 +14,9 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.IsNot.not;
 
+/**
+ * Run on paidDebug build variant
+ */
 @RunWith(AndroidJUnit4.class)
 public class MainActivityGCloudTest {
 
