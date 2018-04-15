5601bde36cec667422780aa742af654aded3c1d0
AndreyRadionov <andyomsk@gmail.com>
2018-04-15 12:33:59 +0600

    Task 5 flavors completed

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityGCloudTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityGCloudTest.java
@@ -1,14 +1,11 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.test.espresso.ViewAssertion;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.concurrent.ExecutionException;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
