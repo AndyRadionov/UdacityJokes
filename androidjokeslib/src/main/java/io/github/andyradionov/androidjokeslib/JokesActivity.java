396270cdf848491812422526fed44f68d3b4fb26
AndreyRadionov <andyomsk@gmail.com>
2018-04-21 18:08:59 +0600

    advert removed from paid version

--- androidjokeslib/src/main/java/io/github/andyradionov/androidjokeslib/JokesActivity.java
+++ androidjokeslib/src/main/java/io/github/andyradionov/androidjokeslib/JokesActivity.java
@@ -1,16 +1,12 @@
 package io.github.andyradionov.androidjokeslib;
 
 import android.content.Intent;
-import android.support.v4.app.NavUtils;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.MenuItem;
 import android.widget.TextView;
-
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 
 public class JokesActivity extends AppCompatActivity {
 
@@ -34,11 +30,6 @@ public class JokesActivity extends AppCompatActivity {
             jokeDisplay.setText(joke);
         }
 
-        AdView mAdView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
     }
 
     @Override
